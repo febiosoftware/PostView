16323bbe8a3d8726b9329bce7f042f2025523405
Gerard Ateshian <ateshian@columbia.edu>
2017-06-28 00:05:20 +0000

    PostView2: modified main to accommodate Mac file opening by double clicking plot file or dropping plot file on PostView2 application icon
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/PostView2@9711 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- PostView2/PostView2.cpp
+++ PostView2/PostView2.cpp
@@ -5,14 +5,39 @@
 #include <QApplication>
 #include "MainWindow.h"
 
+#ifdef __APPLE__
+#include <QFileOpenEvent>
+class MyApplication : public QApplication
+{
+public:
+    MyApplication(int &argc, char **argv)
+    : QApplication(argc, argv)
+    {
+    }
+    void SetMainWindow(CMainWindow* wnd) { m_pWnd = wnd; }
+    
+    bool event(QEvent *event)
+    {
+        if (event->type() == QEvent::FileOpen) {
+            QFileOpenEvent *openEvent = static_cast<QFileOpenEvent *>(event);
+            m_pWnd->OpenFile(openEvent->file(), -1);
+        }
+        
+        return QApplication::event(event);
+    }
+    
+public:
+    CMainWindow* m_pWnd;
+};
+#endif
+
 int main(int argc, char* argv[])
 {
+#ifndef __APPLE__
 	QApplication app(argc, argv);
 
-#ifndef __APPLE__
 	// initialize glew
 	glewInit();
-#endif
 
 	// create the main window
 	CMainWindow wnd;
@@ -24,4 +49,20 @@ int main(int argc, char* argv[])
 	}
 
 	return app.exec();
+#else
+    MyApplication app(argc, argv);
+    
+    // create the main window
+    CMainWindow wnd;
+    wnd.show();
+    
+    app.SetMainWindow(&wnd);
+    
+    if (argc==2)
+    {
+        wnd.OpenFile(argv[1], -1);
+    }
+    
+    return app.exec();
+#endif
 }
