1bbb38ccd39976bff85d05f22eab5359094acd2e
Steve Maas <steve.maas@utah.edu>
2020-03-03 22:58:41 +0000

    PostView2: Fixed bug in Graph window update when options are changed.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/PostView2@13110 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- PostView2/GraphWindow.cpp
+++ PostView2/GraphWindow.cpp
@@ -850,7 +850,7 @@ void CGraphWindow::on_options_optionsChanged()
 	bool autoRng = ui->ops->autoRangeUpdate();
 	ui->plot->setAutoRangeUpdate(autoRng);
 
-	Update(true);
+	Update(false);
 }
 
 //=============================================================================
